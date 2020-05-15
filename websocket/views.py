7e98d1ab393fc2ed7e342e13cdc770364bf3a701
1995kkxl <mrc2017@foxmail.com>
2020-05-15 21:49:44 +0800

    2

---
+++ websocket/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render
+from dwebsocket.decorators import accept_websocket, require_websocket
+from django.http import HttpResponse
+import paramiko
+import psutil
+import time
+
+# def exec_command(comm):
+#     hostname = '192.168.0.162'
+#     username = 'root'
+#     password = 'root'
+#
+#     ssh = paramiko.SSHClient()
+#     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#     ssh.connect(hostname=hostname, username=username, password=password)
+#     stdin, stdout, stderr = ssh.exec_command(comm,get_pty=True)
+#     result = stdout.read()
+#     ssh.close()
+#     yield result
+
+
+@accept_websocket
+def echo_once(request):
+    if not request.is_websocket():  # 判断是不是websocket连接
+        try:  # 如果是普通的http方法
+            message = request.GET['message']
+            return HttpResponse(message)
+        except:
+            return render(request, 'index.html')
+    else:
+        for message in request.websocket:
+            message = message.decode('utf-8')  # 接收前端发来的数据
+            print(message)
+            if message == 'backup_all':#这里根据web页面获取的值进行对应的操作
+                command = 'bash /root/ping.sh'#这里是要执行的命令或者脚本
+
+                # 远程连接服务器
+                hostname = '172.16.2.225'
+                username = 'root'
+                password = '123456'
+
+                ssh = paramiko.SSHClient()
+                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                ssh.connect(hostname=hostname, username=username, password=password)
+                # 务必要加上get_pty=True,否则执行命令会没有权限
+                stdin, stdout, stderr = ssh.exec_command(command, get_pty=True)
+                # result = stdout.read()
+                # 循环发送消息给前端页面
+                while True:
+                    nextline = stdout.readline().strip()  # 读取脚本输出内容
+                    #print(nextline.strip())
+                    request.websocket.send(nextline) # 发送消息到客户端
+                    # 判断消息为空时,退出循环
+                    if not nextline:
+                        break
+                ssh.close()  # 关闭ssh连接
+            elif message =="stop_cmd":
+                print("停止运行")
+                kill_process_with_name("ping")
+                break
+                ssh.close()
+
+            else:
+                request.websocket.send('没权限!!!'.encode('utf-8'))
+
+def kill_process_with_name(process_name):
+    print("-------"+process_name)
+    pid_list = psutil.pids() #遍历所有进程
+    print(pid_list)
+    for pid in pid_list:
+        try:
+            each_pro = psutil.Process(pid)
+            print(each_pro)
+            if process_name.lower() in each_pro.name().lower():
+                print(each_pro.pid)
+                print("进程是%s，pid是%s" % (each_pro.name(),each_pro.pid))
+                time.sleep(1)
+                # 杀死父子进程
+                parent_pid = each_pro.pid
+                parent = psutil.Process(parent_pid)
+                for child in parent.children(recursive=True):  # or parent.children() for recursive=False
+                    child.kill()
+                    print(child)
+                parent.kill()
+                print(parent.status())
+                break
+        except Exception as ret:
+            print(ret)
